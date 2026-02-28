e7e2622fc5dad0700cee3fc1c5989df73a02f427
Fouriosa <b00372274@studentmail.uws.ac.uk>
2026-02-28 13:45:46 +0000

    Ship Movement
    Ship should now appear moving when lever is moved forward. Steering wheel should make the ship appear to move left and right.

---
+++ IEIGame/Assets/Scripts/OutsideSpace.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.XR.Content.Interaction;
+
+public class OutsideSpace : MonoBehaviour
+{
+    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float sideSpeed;
+
+    [SerializeField] private XRLever level;
+    [SerializeField] private XRKnob knob;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
