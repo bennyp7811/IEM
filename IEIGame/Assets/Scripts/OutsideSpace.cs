6d6e0592f42c5b6e2b84941a709d81d2225db671
Fouriosa <b00372274@studentmail.uws.ac.uk>
2026-03-02 17:16:49 +0000

    Script Re-added
    previously the script was empty, should be fixed now

--- IEIGame/Assets/Scripts/OutsideSpace.cs
+++ IEIGame/Assets/Scripts/OutsideSpace.cs
@@ -6,7 +6,7 @@ public class OutsideSpace : MonoBehaviour
     [SerializeField] private float forwardSpeed;
     [SerializeField] private float sideSpeed;
 
-    [SerializeField] private XRLever level;
+    [SerializeField] private XRLever lever;
     [SerializeField] private XRKnob knob;
 
     void Start()
@@ -16,6 +16,13 @@ public class OutsideSpace : MonoBehaviour
 
     void Update()
     {
+
+        float forwardVelocity = forwardSpeed * (lever.value ? 1 : 0);
+        float sideVelocity = sideSpeed * (lever.value ? 1 : 0) * Mathf.Lerp(-1,1, knob.value);
+
+        Vector3 velocity = new Vector3(sideVelocity,0,forwardVelocity);  
+
+        transform.position += velocity * Time.deltaTime;
         
     }
 }
