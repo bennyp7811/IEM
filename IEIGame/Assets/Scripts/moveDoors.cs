5c1b611dcf25b4460247251b17dd04cce7f60ff6
bennyp7811 <156672813+bennyp7811@users.noreply.github.com>
2026-03-18 16:55:36 +0000

    added all the doors and added tv with task sytem

--- IEIGame/Assets/Scripts/moveDoors.cs
+++ IEIGame/Assets/Scripts/moveDoors.cs
@@ -6,12 +6,18 @@ public class moveDoors : MonoBehaviour
     public float moveDistace = 5;
     public float speed = 5;
 
+    public bool keyInsert;
+    
+
     Vector3 startPOS;
     Vector3 targetPOS;
+
     void Start()
     {
         startPOS = transform.position;
         targetPOS = transform.position;
+        keyInsert = false;
+        
        
         
     }
@@ -20,15 +26,19 @@ public class moveDoors : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, targetPOS, speed * Time.deltaTime);
+        
     }
 
     public void moveRight()
     {
+        keyInsert = true;
         targetPOS += Vector3.back * moveDistace;
         StartCoroutine(waitToClose());
+        
     }
     public void moveLeft()
     {
+        keyInsert = true;
         targetPOS += Vector3.forward  * moveDistace;
         StartCoroutine(waitToClose());
     }
@@ -44,6 +54,7 @@ public class moveDoors : MonoBehaviour
 
     public void goBack()
     {
+        keyInsert = false;
 
         targetPOS = startPOS;
 
---
+++ IEIGame/Assets/Scripts/tasks complete.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+public class taskscomplete : MonoBehaviour
+{
+
+    public TMP_Text tasksText;
+    public static int tasks;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        tasks = 0;
+        tasksText.text = (tasks + "/3 tasks complete");
+        
+    }
+  
+
+
+        // Update is called once per frame
+        void Update()
+    {
+        if (tasks >= 1 )
+        {
+            tasksText.text = (tasks + "/3 tasks complete");
+
+        }
+      
+        
+    }
+}
